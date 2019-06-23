a135731ed688f175ee410439c5721ff4688b3a0d
latin3663 <yuuya.k.0508@gmail.com>
2019-06-23 11:01:29 +0900

    aw

--- main.py
+++ main.py
@@ -103,13 +103,14 @@ def message_text(event):
         # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
         status_msg = "なし"
 
-    messages = TemplateSendMessage(alt_text="Buttons template",
-                                   template=ButtonsTemplate(
-                                       thumbnail_image_url=profile.picture_url,
-                                       title=profile.display_name,
-                                       text=f"User Id: {profile.user_id[:5]}...\n"
-                                            f"Status Message: {status_msg}",
-                                       actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
+    # messages = TemplateSendMessage(alt_text="Buttons template",
+    #                                template=ButtonsTemplate(
+    #                                    thumbnail_image_url=profile.picture_url,
+    #                                    title=profile.display_name,
+    #                                    text=f"User Id: {profile.user_id[:5]}...\n"
+    #                                         f"Status Message: {status_msg}",
+    #                                    actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
+    messages = TextMessage(text="Your ID:" + profile.user_id)
 
     line_bot_api.reply_message(event.reply_token, messages=messages)
 
